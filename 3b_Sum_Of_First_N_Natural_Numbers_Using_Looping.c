de35fd89532f1755507a203a8848b822a843938c
Niranjan-KS <niranjan.ks.pro@gmail.com>
2023-12-09 13:40:46 +0530

    commit

--- 3b_Sum_Of_First_N_Natural_Numbers_Using_Looping.c
+++ 3b_Sum_Of_First_N_Natural_Numbers_Using_Looping.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
-int main() {
+int main()
+{
     int n, i, sum = 0;
 
     printf("Enter a positive integer: ");
     scanf("%d", &n);
 
-    for (i = 1; i <= n; ++i) {
+    for (i = 1; i <= n; ++i)
+    {
         sum += i;
     }
 
