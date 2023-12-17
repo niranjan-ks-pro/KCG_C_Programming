e78de6726550c0ec14654011ed7083fda3b3bd63
Niranjan-KS <niranjan.ks.pro@gmail.com>
2023-12-17 12:55:18 +0530

    commit

--- 8a_Pointer_To_Pointer.c
+++ 8a_Pointer_To_Pointer.c
@@ -1,2 +1,21 @@
 #include <stdio.h>
-    
+#include <stdio.h>
+
+int main()
+{
+
+    int a;
+    int *ptr;
+    printf("Pointer example program:Print Pointer Adress\n");
+
+    a = 10;
+    ptr = &a;
+    printf("[a]:Value of A =%d", a);
+    printf("\n[*ptr]:Value of A =%d", *ptr);
+    printf("\n[&a]:Value of A =%p", &a);
+    printf("\n[ptr]:Value of A =%p", ptr);
+    printf("\n[&ptr]:Value of ptr =%p", &ptr);
+    printf("\n[ptr]:Value of ptr =%p", ptr);
+
+    return 0;
+}
